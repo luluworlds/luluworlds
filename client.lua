e169e248710bd6a0e89c637d1a3bff5a6e8723a6
ChillerDragon <ChillerDragon@gmail.com>
2024-07-12 11:37:01 +0800

    connecting to server works

--- client.lua
+++ client.lua
@@ -56,9 +56,39 @@ local function version_and_password()
 	return header .. msg
 end
 
+local function ready()
+	local header = string.char(0x00, 0x01, 0x01) .. teeworlds_client.server_token
+	local msg = string.char(
+		0x40, 0x01, 0x02, 0x25
+	)
+	return header .. msg
+end
+
+local function start_info()
+	local header = string.char(0x00, 0x04, 0x01) .. teeworlds_client.server_token
+	local msg = string.char(
+		0x41, 0x19, 0x03, 0x36, 0x6E, 0x61, 0x6D, 0x65, 0x6C, 0x65, 0x73, 0x73,
+		0x20, 0x74, 0x65, 0x65, 0x00, 0x00, 0x40, 0x73, 0x70, 0x69, 0x6B, 0x79,
+		0x00, 0x64, 0x75, 0x6F, 0x64, 0x6F, 0x6E, 0x6E, 0x79, 0x00, 0x00, 0x73,
+		0x74, 0x61, 0x6E, 0x64, 0x61, 0x72, 0x64, 0x00, 0x73, 0x74, 0x61, 0x6E,
+		0x64, 0x61, 0x72, 0x64, 0x00, 0x73, 0x74, 0x61, 0x6E, 0x64, 0x61, 0x72,
+		0x64, 0x00, 0x01, 0x01, 0x00, 0x01, 0x01, 0x01, 0xA0, 0xAC, 0xDD, 0x04,
+		0xBD, 0xD2, 0xA9, 0x85, 0x0C, 0x80, 0xFE, 0x07, 0x80, 0xC0, 0xAB, 0x05,
+		0x9C, 0xDE, 0xAA, 0x05, 0x9E, 0xC9, 0xE5, 0x01
+	)
+	return header .. msg
+end
+
+local function enter_game()
+	local header = string.char(0x00, 0x06, 0x01) .. teeworlds_client.server_token
+	local msg = string.char(
+		0x40, 0x01, 0x04, 0x27
+	)
+	return header .. msg
+end
+
 local socket = require("socket")
 local udp = assert(socket.udp())
-local data
 
 udp:settimeout(1)
 assert(udp:setsockname("*", 0))
@@ -66,21 +96,54 @@ assert(udp:setpeername("127.0.0.1", 8303))
 
 assert(udp:send(ctrl_msg_token()))
 
-while true do
-	data = udp:receive()
+local function on_data(data)
 	print(STR_HEX(data))
-	-- control message
-	if data:byte(1) == 0x04 then
-		local ctrl = data:byte(8)
+	if #data < 8 then
+		print("ignoring too short packet")
+		return
+	end
+	local payload = data:sub(8)
+	if data:byte(1) == 0x04 then -- control message
+		local ctrl = payload:byte(1)
+		print("got ctrl: " .. ctrl)
 		if ctrl == CTRL_TOKEN then
-			teeworlds_client.server_token = data:sub(9, 12)
+			teeworlds_client.server_token = payload:sub(2)
 			print("got token: " .. STR_HEX(teeworlds_client.server_token))
 			assert(udp:send(ctrl_connect()))
 		elseif ctrl == CTRL_ACCEPT then
 			print("got accept")
 			assert(udp:send(version_and_password()))
+		elseif ctrl == CTRL_CLOSE then
+			io.write("got disconnect from server")
+			local reason = payload:sub(2)
+			if #reason > 0 then
+				print(" (" .. payload:sub(2) .. ")")
+			else
+				print("")
+			end
+			os.exit(0)
+		end
+	else -- sys and game messages
+		local hack_chunk_header = payload:sub(2, 4)
+		print("chunk headrer: " .. STR_HEX(hack_chunk_header))
+		if hack_chunk_header == string.char(0x3A, 0x01, 0x05) then
+			print("got map change sending ready")
+			assert(udp:send(ready()))
+		elseif  hack_chunk_header == string.char(0x02, 0x02, 0x02) then
+			print("got motd, server settings and con ready")
+			assert(udp:send(start_info()))
+		elseif  hack_chunk_header == string.char(0x01, 0x05, 0x16) then
+			print("assume this is ready to enter xd")
+			assert(udp:send(enter_game()))
 		end
 	end
-	break
-	-- 04 00 00 01 02 03 04 05 0E 8A 02 01
+	-- needed for neovim
+	io.flush()
+end
+
+while true do
+	local data = udp:receive()
+	if data ~= nil then
+		on_data(data)
+	end
 end
