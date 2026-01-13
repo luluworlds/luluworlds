001b891cd8a744ad1eb2771c0aa5b9718fad394c
ChillerDragon <chillerdragon@gmail.com>
2026-01-13 15:36:06 +0100

    Remove bitop-lua depency by copy pasting the needed code
    It feels a bit janky to steal code like this.
    But I don't even know who is the actual author of this 15 year old lib.
    The luarocks package I was using seemingly got deleted.
    
    So this fixes the build.
    
    I also tried the "bit32" module using `require("bit32")` but it fails
    the tests. Particularly this one `assert(u:get_int() == -128)`
    
    If ran with luajit the builtin `bit` global provides correctly working
    bit ops but that would drop pure lua support.
    
    There are some concerns about this code not working in lua5.3 anymore
    https://www.freelists.org/post/luajit/Port-bitop-to-53,1
    which sounds annoying but I could not reproduce any actual issues.
    
    Ideally this should use the known to be fast and working luajit builtin
    if it can. See issue #2 for that.
    
    Closed #1

--- src/bits.lua
+++ src/bits.lua
@@ -1,12 +1,55 @@
-local bit = require 'bitop.funcs'
-local bit32 = require 'bitop.funcs'.bit32
+-- this code is from the bitop-lua library
+-- credits go to https://github.com/NicholasMindieta
+--
+-- https://github.com/luluworlds/bitop-lua/blob/8146f0b323f55f72eacbb2a8310922d50ae75ddf/src/bitop/funcs.lua#L5-L54
+-- https://github.com/RobloxEadmond/bitop-lua/blob/8146f0b323f55f72eacbb2a8310922d50ae75ddf/src/bitop/funcs.lua#L5-L54
+
+local MOD = 2^32
+
+local function memoize(f)
+  local mt = {}
+  local t = setmetatable({}, mt)
+  function mt:__index(k)
+    local v = f(k)
+    t[k] = v
+    return v
+  end
+  return t
+end
+
+local function make_bitop_uncached(t, m)
+  local function bitop(a, b)
+    local res,p = 0,1
+    while a ~= 0 and b ~= 0 do
+      local am, bm = a%m, b%m
+      res = res + t[am][bm]*p
+      a = (a - am) / m
+      b = (b - bm) / m
+      p = p*m
+    end
+    res = res + (a+b) * p
+    return res
+  end
+  return bitop
+end
+
+local function make_bitop(t)
+  local op1 = make_bitop_uncached(t, 2^1)
+  local op2 = memoize(function(a)
+    return memoize(function(b)
+      return op1(a, b)
+    end)
+  end)
+  return make_bitop_uncached(op2, 2^(t.n or 1))
+end
 
 local function bit_bxor(a, b)
-    return bit.bxor(a, b)
+    local bxor = make_bitop {[0]={[0]=0,[1]=1},[1]={[0]=1,[1]=0}, n=4}
+    return bxor(a, b)
 end
 
 local function bit32_not(n)
-    return bit32.bnot(n)
+    return (-1 - n) % MOD
 end
 
 -- https://stackoverflow.com/a/25594410
@@ -32,6 +75,7 @@ local function bit_and(a,b)
 end
 
 -- https://stackoverflow.com/a/6026257
+
 local function lshift(x, by)
   return x * 2 ^ by
 end
